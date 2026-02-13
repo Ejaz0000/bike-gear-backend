1d53131f942659735972075dd2c258aba300eda3
Ejaz Ahmed <120748404+Ejaz0000@users.noreply.github.com>
2026-02-14 01:12:41 +0600

    Switch to Pipfile for Vercel deployment
    - Remove requirements.txt
    - Use Pipfile.lock for dependency management
    - Update buildCommand to use pipenv
    - Update build_files.sh for pipenv
    - Allow Pipfile and Pipfile.lock in deployment

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install dependencies with user flag to avoid PEP 668 issues
-pip install --user -r requirements.txt
+# Install dependencies using pipenv
+pipenv install --deploy
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+pipenv run python manage.py collectstatic --noinput --clear
